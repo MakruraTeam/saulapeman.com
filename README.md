9ffe1b847abb32744a04c2225256d599961dc238
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-10-06 10:23:00 +0200

    feat: Initialize frontend

---
+++ README.md
@@ -0,0 +1,14 @@
+# Frontend
+
+## Stack
+
+- Node v24.9.0
+- npm v11.6.0
+
+## How to run
+
+```bash
+cd frontend
+npm install
+npm run serve
+```
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <button class="btn btn-primary">Save</button>
+    <h1>test</h1>
+  </div>
+</template>
