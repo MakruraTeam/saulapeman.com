1102ba879d28229bc1e422d8391baffc09100a51
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-10-08 10:15:38 +0200

    feat: add login page for cms

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import DesktopView from '@/views/Desktop.vue';
+import LoginView from '@/views/cms/Login.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: DesktopView,
+  },
+  {
+    path: '/admin-panel',
+    name: 'admin-panel',
+    component: LoginView,
   },
 ];
 
