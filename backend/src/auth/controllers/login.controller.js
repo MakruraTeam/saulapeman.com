4d727726c9ed3d44f7fe762664d5bed2183489b2
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-10-08 12:02:32 +0200

    feat: add login route

---
+++ backend/src/auth/controllers/login.controller.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import UserSchema from '../schemas/user.schema.js';
+
+export const loginController = async (req, res) => {
+  const { username, password, rememberMe = false } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+
+  try {
+    const user = await UserSchema.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const token = rememberMe
+      ? jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+      : jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '2h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
