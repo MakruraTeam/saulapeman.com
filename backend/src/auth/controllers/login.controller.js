735c9a347c7b4890c9249785c9726b99c91b3dbb
Paweł Bińkowski <pawel.binkowski@over-cloud.pl>
2025-10-08 12:34:36 +0200

    feat: add logout option

--- backend/src/auth/controllers/login.controller.js
+++ backend/src/auth/controllers/login.controller.js
@@ -19,8 +19,7 @@ export const loginController = async (req, res) => {
       return res.status(401).json({ error: 'Invalid username or password' });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: rememberMe ? '30d' : '2h' });
-
+    const token = jwt.sign({ id: user._id, tokenHash: user.tokenHash }, process.env.JWT_SECRET, { expiresIn: rememberMe ? '30d' : '2h' });
     res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
--- backend/src/auth/middlewares/userAuth.middlewares.js
+++ backend/src/auth/middlewares/userAuth.middlewares.js
@@ -10,11 +10,14 @@ export const userAuthorization = async (req, res, next) => {
   const token = auth.split(' ')[1];
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    console.log('decoded', decoded);
     if (err) return res.status(401).json({ error: 'Invalid token' });
 
     const user = await User.findById(decoded.id);
     if (!user) return res.status(401).json({ error: 'Invalid token' });
+
+    if (decoded.tokenHash !== user.tokenHash) {
+      return res.status(401).json({ error: 'Token no longer valid (logged out)' });
+    }
 
     req.user = user;
     next();
